ae7ba2530e09ecd48ed2a377430969dc23f0b184
devsamin <abdullahalsamin79@gmail.com>
2025-02-27 22:57:55 -0800

    change added

--- job_board/settings.py
+++ job_board/settings.py
@@ -31,11 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://find-job-v4mq.onrender.com",
-    "http://127.0.0.1:5500",
-    "http://127.0.0.1:8000",
-]
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com', 'http://127.0.0.1:8000']
 
 
 # Application definition
